130c2f26d2b9d3ddd5ddeabdbe92d81fafde81bf
dlenhart <dlenhart@gmail.com>
2021-04-14 11:36:15 -0400

    New content

--- js/snowyworks-app.js
+++ js/snowyworks-app.js
@@ -15,7 +15,7 @@ function loadComicCatalog(resultsDiv = "results", debug = false, loader){
 
   turnOffLoader.hide();
 
-  $.getJSON('data/comic-data.json?v=12', function(data) {
+  $.getJSON('data/comic-data.json?v=14', function(data) {
     if(debug) { console.log(Object.keys(data).length); }
     $.each(data, function(key, value) {
       let content = '';
