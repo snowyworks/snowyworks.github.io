c1edf56fb25a6b61de35ec3de8f25beb661972d6
dlenhart <dlenhart@gmail.com>
2025-12-19 12:49:44 -0500

    adding book section.

--- js/snowyworks-app.js
+++ js/snowyworks-app.js
@@ -9,23 +9,31 @@
 function loadComicCatalog(resultsDiv = "results", debug = false, loader, type){
   let stagingArea = $("#" + resultsDiv);
   let turnOffLoader = $("#" + loader);
-  let displayCatelogueFile = "";
-  let displayImagePath = "";
 
-  if(type == "full") {
-    displayCatelogueFile = "../data/comic-data-full.json";
-    displayImagePath = "../images/";
-  } else {
-    displayCatelogueFile = "data/comic-data-home.json";
-    displayImagePath = "images/";
-  }
+  const catalogueConfig = {
+    full: {
+      catalogueFile: "../data/comic-data-full.json",
+      imagePath: "../images/"
+    },
+    books: {
+      catalogueFile: "data/comic-data-books.json",
+      imagePath: "images/"
+    },
+    default: {
+      catalogueFile: "data/comic-data-home.json",
+      imagePath: "images/"
+    }
+  };
+
+  const config = catalogueConfig[type] || catalogueConfig.default;
+  const { catalogueFile, imagePath } = config;
 
   var ts = new Date().getTime();
   var data = {_: ts};
 
   turnOffLoader.hide();
 
-  $.getJSON(displayCatelogueFile, function(data) {
+  $.getJSON(catalogueFile, function(data) {
     if(debug) { console.log(Object.keys(data).length); }
     $.each(data, function(key, value) {
       let content = '';
@@ -34,7 +42,7 @@ function loadComicCatalog(resultsDiv = "results", debug = false, loader, type){
 
       content += "<div class=\"col-md-3 text-center\">";
       content += "<a href=\"\" class=\"open-comicDialog\" data-title=\"" + value.title + "\" data-id=\"" + value.id + "\" data-toggle=\"modal\" data-target=\"#fsModal\">";
-      content += "<img src=\"" + displayImagePath + value.image + "\" alt=\"\" class=\"compImg shadowImg\" />";
+      content += "<img src=\"" + imagePath + value.image + "\" alt=\"\" class=\"compImg shadowImg\" />";
       content += "</a><br />";
       content += "<button class=\"btn btn-primary btn-spacing open-comicDialog\" data-title=\"" + value.title + "\" data-id=\"" + value.id + "\" data-toggle=\"modal\" data-target=\"#fsModal\">" + value.label + "</button>";
       content += "</div>";
